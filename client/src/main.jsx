fbc5e7ac56782126911ceeab6d49bbed0ab803b9
Deiby Arango <deiby.arango@hotmail.com>
2025-07-28 22:26:54 -0500

    refactor: actualizar archivos de configuración principal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import App from './App.jsx';
+import AboutPage from './pages/about/index.jsx';
 import NotFoundPage from './pages/common/NotFoundPage.jsx';
 import ContactPage from './pages/contact/index.jsx';
+import ExperiencePage from './pages/experience/index.jsx';
 import Homepage from './pages/homepage/index.jsx';
+import ProjectsPage from './pages/projects/index.jsx';
+import ProjectDetailPage from './pages/projects/ProjectDetailPage.jsx';
+import SkillsPage from './pages/skills/index.jsx';
 
 // Definición de las rutas de la aplicación
 const router = createBrowserRouter([
@@ -19,6 +24,26 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <Homepage />,
+            },
+            {
+                path: 'about',
+                element: <AboutPage />,
+            },
+            {
+                path: 'projects',
+                element: <ProjectsPage />,
+            },
+            {
+                path: 'projects/:id',
+                element: <ProjectDetailPage />,
+            },
+            {
+                path: 'skills',
+                element: <SkillsPage />,
+            },
+            {
+                path: 'experience',
+                element: <ExperiencePage />,
             },
             {
                 path: 'contact',
